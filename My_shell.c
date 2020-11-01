ae1b3c8ffcc1799b47f151d105a20325501816d2
toannguyen1101 <58873509+toannguyen1101@users.noreply.github.com>
2020-11-01 11:00:49 +0700

    Update My_shell.c

--- My_shell.c
+++ My_shell.c
@@ -12,6 +12,7 @@
 char* takeinput() {
     char* line = NULL;
     int size = 0;
+    printf("T_Shell>> ");
    
     getline(&line, &size, stdin);
 
@@ -19,27 +20,65 @@ char* takeinput() {
 }
 #define _delim " \t\r\n\a"
 char** split_line(char*line) {
-    int size = 64, position = 0;
+    
+    int size = 64;
+    int pos = 0;
     char** tokens = (char**)malloc(size * sizeof(char*));
     char* token;
 
     token = strtok(line, _delim);
     while (token != NULL) {
-        tokens[position] = token;
-        position++;
+        tokens[pos] = token;
+        pos++;
 
-        if (position >= size) {
+        if (pos >= size) {
             size += 64;
             tokens = (char**)realloc(tokens, size * sizeof(char*));
         }
 
         token = strtok(NULL, _delim);
     }
-    tokens[position] = NULL;
+    
+    tokens[pos] = NULL;
     return tokens;
+}
+
+void execArg(char **args) {
+    pid_t pid, wpid;
+
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execvp(args[0], args) == -1) {
+            perror("exevcp failed");
+        }
+        exit(EXIT_FAILURE);
+    }
+    else if (pid < 0) {
+        perror("Error forking");
+        exit(EXIT_FAILURE);
+    }
+    else {
+        // Parent process
+        waitpid(pid, NULL, 0);
+    }
+
+    return;
+}
+
+void shell_loop() {
+    char* line;
+    char** args;
+    do {
+        line = takeinput();
+        args = split_line(line);
+        execArg(args);
+        free(args);
+        free(line);
+    } while (strcmp(args[0], "exit") != 0);
 }
 
 int main()
 {
-    
+    shell_loop();
 }
